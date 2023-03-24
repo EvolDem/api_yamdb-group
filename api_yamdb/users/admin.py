767101da2185c2ce765ec29fa09f1a741832b0ff
unknown <heart.of.bass@gmail.com>
2023-03-24 19:12:43 +0300

    Добавлена админка модели Users.

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from .models import CustomUser
 
-admin.register(CustomUser)
+from users.models import CustomUser
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'name',
+                    'email',
+                    'role',
+                    'bio',
+                    'first_name',
+                    'last_name',
+                    )
+    list_display_links = ('id', 'name', 'role',)
+    search_fields = ('first_name', 'last_name',)
+    list_filter = ('first_name', 'last_name', 'role',) 
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
